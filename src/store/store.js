aff00978493b092c812a05be8a2468e67fb4af1d
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-11-06 15:10:18 +0500

    store update

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 import authorization from "@/store/modules/authorization";
+import product from "./modules/product";
 
 const store = createStore({
 	modules: {
 		authorization: authorization,
+		product: product,
 	},
 });
 

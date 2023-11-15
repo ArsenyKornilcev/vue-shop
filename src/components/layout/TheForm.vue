60ecd0b10104448b717ba0d66a1a4cdacf311695
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-11-15 18:54:24 +0500

    start making form component

---
+++ src/components/layout/TheForm.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<form @submit="submit">
+			<div
+				class="column"
+				v-for="input in inputs"
+				:key="input.id">
+				<label :for="input.id">{{ input.label }}</label>
+				<input
+					:id="input.id"
+					:type="input.type"
+					:placeholder="input.placeholder" />
+			</div>
+			<div
+				class="column"
+				v-for="textarea in textareas"
+				:key="textarea.id">
+				<label :for="textarea.id">{{ textarea.label }}</label>
+				<textarea
+					:id="textarea.id"
+					:placeholder="textarea.placeholder"></textarea>
+			</div>
+
+			<div class="buttons">
+				<button>{{ submitBtnText }}</button>
+				<button
+					type="reset"
+					v-if="resetBtnText">
+					{{ resetBtnText }}
+				</button>
+			</div>
+		</form>
+	</div>
+</template>
+<script>
+	export default {
+		props: {
+			inputs: Array,
+			textareas: Array,
+			resetBtnText: String,
+			submitBtnText: String,
+		},
+		methods: {
+			submit() {
+				// this.$emit("submit", newProduct);
+			},
+		},
+	};
+</script>
+<style scoped>
+	.column {
+		display: flex;
+		flex-flow: column wrap;
+	}
+	.buttons {
+		display: flex;
+		gap: 10px;
+	}
+</style>
--- src/components/pages/AdminPage.vue
+++ src/components/pages/AdminPage.vue
@@ -1,9 +1,24 @@
 <template>
-	<div></div>
+	<div>
+		<h2 class="text-center">This is admin page</h2>
+
+		<the-form
+		@submit="addProduct"
+		></the-form>
+	</div>
 </template>
 <script>
+	import TheForm from "../layout/TheForm.vue";
 	export default {
 		name: "AdminPage",
+		components: {
+			TheForm,
+		},
+		methods: {
+			addProduct() {
+
+			}
+		}
 	};
 </script>
 <style></style>

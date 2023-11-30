3781768e33c4cd0305f725bac002cfd3874dbfae
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2023-11-30 14:25:12 +0500

    ui: form fix, admin page fill

--- src/components/layout/TheForm.vue
+++ src/components/layout/TheForm.vue
@@ -1,28 +1,47 @@
 <template>
 	<div>
-		<form @submit="submit">
-			<div
-				class="column"
-				v-for="input in inputs"
-				:key="input.id">
-				<label :for="input.id">{{ input.label }}</label>
-				<input
-					:id="input.id"
-					:type="input.type"
-					:placeholder="input.placeholder" />
-			</div>
-			<div
-				class="column"
-				v-for="textarea in textareas"
-				:key="textarea.id">
-				<label :for="textarea.id">{{ textarea.label }}</label>
-				<textarea
-					:id="textarea.id"
-					:placeholder="textarea.placeholder"></textarea>
+		<form @submit.prevent>
+			<div class="inputs">
+				<div
+					class="column"
+					v-for="input in inputs"
+					:key="input.id">
+					<label
+						:for="input.id"
+						v-if="input.label"
+						>{{ input.label }}</label
+					>
+					<input
+						:id="input.id"
+						:type="input.type"
+						:placeholder="input.placeholder" />
+				</div>
+				<div
+					class="column"
+					v-for="textarea in textareas"
+					:key="textarea.id">
+					<label
+						:for="textarea.id"
+						v-if="textarea.label"
+						>{{ textarea.label }}</label
+					>
+					<textarea
+						:id="textarea.id"
+						:placeholder="textarea.placeholder"></textarea>
+				</div>
 			</div>
 
 			<div class="buttons">
-				<button>{{ submitBtnText }}</button>
+				<button
+					@click="submit"
+					v-if="!submitItemBtn">
+					{{ submitBtnText }}
+				</button>
+				<button
+					@click="submitItem"
+					v-else>
+					{{ submitBtnText }}
+				</button>
 				<button
 					type="reset"
 					v-if="resetBtnText">
@@ -34,15 +53,28 @@
 </template>
 <script>
 	export default {
+		data() {
+			return {
+				newProduct: null,
+			};
+		},
 		props: {
 			inputs: Array,
 			textareas: Array,
 			resetBtnText: String,
 			submitBtnText: String,
+			submitItemBtn: Boolean, // if defined form will provide item on submit action
 		},
 		methods: {
+			submitItem() {
+				this.newProduct = {
+					...this.inputs,
+					...this.textareas,
+				};
+				this.$emit("custom-submit", this.newProduct);
+			},
 			submit() {
-				// this.$emit("submit", newProduct);
+				this.$emit("custom-submit");
 			},
 		},
 	};
@@ -51,9 +83,26 @@
 	.column {
 		display: flex;
 		flex-flow: column wrap;
+		gap: 5px;
 	}
 	.buttons {
 		display: flex;
+		gap: 10px;
+	}
+
+	.inputs {
+		display: flex;
+		flex-flow: column wrap;
+		gap: 10px;
+	}
+
+	form .buttons {
+		margin-top: 5px;
+	}
+
+	form {
+		display: flex;
+		flex-flow: column wrap;
 		gap: 10px;
 	}
 </style>
--- src/components/pages/AdminPage.vue
+++ src/components/pages/AdminPage.vue
@@ -1,24 +1,50 @@
 <template>
 	<div>
-		<h2 class="text-center">This is admin page</h2>
+		<h2 class="text-center">This is a admin page</h2>
 
 		<the-form
-		@submit="addProduct"
-		></the-form>
+			@custom-submit="addProduct"
+			resetBtnText="Reset"
+			submitItemBtn="true"
+			submitBtnText="Submit"
+			:inputs="inputs"
+			:textareas="textareas"></the-form>
 	</div>
 </template>
 <script>
 	import TheForm from "../layout/TheForm.vue";
 	export default {
 		name: "AdminPage",
+		data() {
+			return {
+				inputs: [
+					{
+						id: "i1",
+						label: "Title",
+						type: "text",
+					},
+					{
+						id: "i2",
+						label: "Price",
+						type: "number",
+					},
+				],
+				textareas: [
+					{
+						id: "t1",
+						label: "Description",
+					},
+				],
+			};
+		},
 		components: {
 			TheForm,
 		},
 		methods: {
 			addProduct() {
-
-			}
-		}
+				alert("submitted");
+			},
+		},
 	};
 </script>
 <style></style>

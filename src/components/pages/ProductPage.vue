611917f5e445b8940a3ba41b25203ef11a92cc8d
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2024-02-18 22:15:22 +0500

    full info about product in product page, but without styles

--- src/components/pages/ProductPage.vue
+++ src/components/pages/ProductPage.vue
@@ -1,12 +1,37 @@
 <template>
-	<div>
-		<h1>{{ currentProductId }}</h1>
-	</div>
+	<the-card>
+		<div>
+			<img
+				class="product__img"
+				:src="currentProduct.image"
+				alt="" />
+			<div class="product__title">{{ currentProduct.title }}</div>
+			<div class="product__description">
+				{{ currentProduct.description }}
+			</div>
+			<div class="product__price">{{ currentProduct.price }}</div>
+
+			<div class="btns">
+				<the-button
+					positive-secondary
+					rounded
+					@click="addProduct">
+					Add to Cart
+				</the-button>
+			</div>
+		</div>
+	</the-card>
 </template>
 
 <script>
+	import TheCard from "../layout/TheCard.vue";
+
 	export default {
 		name: "ProductPage",
+
+		components: {
+			TheCard,
+		},
 
 		computed: {
 			allProduct() {
@@ -21,6 +46,14 @@
 
 			currentProductId() {
 				return this.$route.params.id;
+			},
+		},
+
+		methods: {
+			addProduct() {
+				const payload = { id: this.currentProduct.id };
+
+				this.$store.dispatch("cart/addItem", payload);
 			},
 		},
 	};

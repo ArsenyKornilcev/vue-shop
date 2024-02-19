7ba956fddab8655f5f599dbc4119a683bb34e9ed
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2024-02-19 21:48:10 +0500

    styles for product page

--- src/components/pages/ProductPage.vue
+++ src/components/pages/ProductPage.vue
@@ -1,23 +1,36 @@
 <template>
 	<the-card>
 		<div>
-			<img
-				class="product__img"
-				:src="currentProduct.image"
-				alt="" />
-			<div class="product__title">{{ currentProduct.title }}</div>
-			<div class="product__description">
-				{{ currentProduct.description }}
-			</div>
-			<div class="product__price">{{ currentProduct.price }}</div>
+			<div class="row">
+				<div class="column column_img">
+					<img
+						class="product__img"
+						:src="currentProduct.image"
+						alt="" />
+				</div>
 
-			<div class="btns">
-				<the-button
-					positive-secondary
-					rounded
-					@click="addProduct">
-					Add to Cart
-				</the-button>
+				<div class="column column_text">
+					<div class="product__title">
+						{{ currentProduct.title }}
+					</div>
+
+					<div class="product__description">
+						{{ currentProduct.description }}
+					</div>
+
+					<div class="product__price">
+						<span>${{ currentProduct.price }}</span>
+					</div>
+
+					<div class="btns">
+						<the-button
+							positive-secondary
+							rounded
+							@click="addProduct">
+							Add to Cart
+						</the-button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</the-card>
@@ -59,4 +72,37 @@
 	};
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+	.row
+		display: grid
+		grid-template-columns: 6fr 4fr
+		gap: 20px
+		justify-content: center
+
+	.column
+		img
+			width: 100%
+			object-fit: cover
+			border-radius: 15px
+			display: block
+
+		&_text
+			display: flex
+			flex-flow: column wrap
+			gap: 10px
+
+	.product
+		&__title
+			color: #1cad88
+			font-weight: bold
+			font-size: 24px
+
+	span
+		border-radius: 15px
+		background: #f3dd32
+		padding: 4px 16px
+		font-weight: bold
+
+	.btns
+		margin-top: 10px
+</style>
